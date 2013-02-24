447c4ce32b7203f7aa716ef11a813b2a0201298c
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-02-24 21:27:15 +0000

    exercise 1.5 complete, dragon can stand on top of sword

--- src/Maze.java
+++ src/Maze.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 
 public class Maze {
-	private int size_h = 10;
-	private int size_v = 10;
+	private static final int SIZE_H = 10;
+	private static final int SIZE_V = 10;
 	Dragon dragon = new Dragon(); 
 	Hero hero = new Hero();
 	char wall = 'X';
@@ -16,9 +16,9 @@ public class Maze {
 	char weapon = 'E';
 	static boolean gameOver = false;
 	static String gameOverMessage = "Game Over";
-	static int dragonMoves = 5;
+	private static final int DRAGONCHOICES = 5;
 
-	private char[][] positions = new char[size_h][size_v];
+	private char[][] positions = new char[SIZE_H][SIZE_V];
 
 	public void spawnDragon(int x, int y){
 		dragon.x = x;
@@ -35,7 +35,7 @@ public class Maze {
 	public void startPredefinedMaze(){
 		for (int x = 0; x < positions.length; x++) {
 			for (int y = 0; y < positions.length; y++) {
-				if ( x == 0 || x == size_h-1  || y == 0 || y == size_v-1){
+				if ( x == 0 || x == SIZE_H-1  || y == 0 || y == SIZE_V-1){
 					positions[x][y] = wall;
 				} else if(y == 2 || y == 3 || y == 5 || y == 7){
 					positions[x][y] = wall;
@@ -57,11 +57,14 @@ public class Maze {
 		positions[8][7] = empty;
 		positions[5][9] = exit;
 		positions[8][1] = weapon;
+		positions[2][4] = weapon;
+		positions[4][4] = weapon;
+
 	}
 
 	public void printMaze(){
-		for (int x = 0; x < size_h; x++) {
-			for (int y = 0; y < size_v; y++) {
+		for (int x = 0; x < SIZE_H; x++) {
+			for (int y = 0; y < SIZE_V; y++) {
 				System.out.print(positions[x][y]);
 				System.out.print(space);
 			}
@@ -69,13 +72,14 @@ public class Maze {
 		}
 	}
 
-	public boolean checkValidPosition(int x, int y){
-		if(x <= 0 || x >= size_v || y <= 0 || y >= size_h){
+	public boolean checkPosition(int x, int y){
+		if(x <= 0 || x >= SIZE_V || y <= 0 || y >= SIZE_H){
 			return false;
 		}
 		if (positions[x][y] == dragon.image ) {
 			if(hero.isArmed){
 				// dragon was slain
+				dragon.die();
 				System.out.println("You have killed the dragon!");
 				return true;
 			} else {
@@ -113,7 +117,7 @@ public class Maze {
 
 		switch (input) {
 		case "w": 
-			if (checkValidPosition(hero.x-1, hero.y)){
+			if (checkPosition(hero.x-1, hero.y)){
 				positions[hero.x][hero.y] = empty;
 				hero.x -=1;
 				positions[hero.x][hero.y] = hero.image;
@@ -121,7 +125,7 @@ public class Maze {
 			}
 			break;
 		case "a":
-			if (checkValidPosition(hero.x, hero.y-1)){
+			if (checkPosition(hero.x, hero.y-1)){
 				positions[hero.x][hero.y] = empty;
 				hero.y -=1;
 				positions[hero.x][hero.y] = hero.image;	
@@ -129,7 +133,7 @@ public class Maze {
 			}
 			break;
 		case "s":
-			if (checkValidPosition(hero.x+1, hero.y)){
+			if (checkPosition(hero.x+1, hero.y)){
 				positions[hero.x][hero.y] = empty;
 				hero.x +=1;
 				positions[hero.x][hero.y] = hero.image;
@@ -137,7 +141,7 @@ public class Maze {
 			}
 			break;
 		case "d" :
-			if (checkValidPosition(hero.x, hero.y+1)){
+			if (checkPosition(hero.x, hero.y+1)){
 				positions[hero.x][hero.y] = empty;
 				hero.y +=1;
 				positions[hero.x][hero.y] = hero.image;
@@ -152,50 +156,68 @@ public class Maze {
 	}
 	
 	public boolean checkDragonPosition(int x, int y){
-		if(x <= 0 || x >= size_v || y <= 0 || y >= size_h){
+		if(x <= 0 || x >= SIZE_V || y <= 0 || y >= SIZE_H){
 			return false;
-		}
-		if (positions[x][y] == empty)
+		} 
+		if (positions[x][y] == weapon ) {
+			dragon.toggleInSword();
 			return true;
+		}
+		if (positions[x][y] == empty && dragon.isOnSword){
+			dragon.toggleInSword();
+			return true;
+		}
+		if (positions[x][y] == empty ){
+			return true;
+		}
 		else return false;
 	}
 	
 	public void moveDragon(){
+		if ( ! dragon.isAlive ){
+			return;
+		}
 		Random r = new Random();
 		boolean done = false;
+		char block;
+		if (dragon.isOnSword){
+			block = weapon;
+		} else {
+			block = empty;
+		}
 		while (!done){
-			int move = r.nextInt(dragonMoves);	
+			int move = r.nextInt(DRAGONCHOICES);	
 			switch (move) {
 			case 0:
 				done = true;
 				break;
 			case 1: // w
 				if (checkDragonPosition(dragon.x-1, dragon.y)){
-					positions[dragon.x][dragon.y] = empty;
+ 					positions[dragon.x][dragon.y] = block;
 					dragon.x -=1;
 					positions[dragon.x][dragon.y] = dragon.image;
 					done = true;
 				}
 				break;
 			case 2: // a
-				if (checkValidPosition(dragon.x, dragon.y-1)){
-					positions[dragon.x][dragon.y] = empty;
+				if (checkDragonPosition(dragon.x, dragon.y-1)){
+					positions[dragon.x][dragon.y] = block;
 					dragon.y -=1;
 					positions[dragon.x][dragon.y] = dragon.image;	
 					done = true;
 				}
 				break;
 			case 3: // s
-				if (checkValidPosition(dragon.x+1, dragon.y)){
-					positions[dragon.x][dragon.y] = empty;
+				if (checkDragonPosition(dragon.x+1, dragon.y)){
+					positions[dragon.x][dragon.y] = block;
 					dragon.x +=1;
 					positions[dragon.x][dragon.y] = dragon.image;
 					done = true;
 				}
 				break;
 			case 4: // d
-				if (checkValidPosition(dragon.x, dragon.y+1)){
-					positions[dragon.x][dragon.y] = empty;
+				if (checkDragonPosition(dragon.x, dragon.y+1)){
+					positions[dragon.x][dragon.y] = block;
 					dragon.y +=1;
 					positions[dragon.x][dragon.y] = dragon.image;
 					done = true;
@@ -206,6 +228,34 @@ public class Maze {
 			}
 		}		
 
+	}
+	
+	public char[][] generateMaze(int width, int height){
+		char[][] maze = new char [width][height];
+		/*
+create a CellStack (LIFO) to hold a list of cell locations  
+set TotalCells = number of cells in grid  
+choose a cell at random and call it CurrentCell  
+set VisitedCells = 1  
+
+while VisitedCells < TotalCells 
+	find all neighbors of CurrentCell with all walls intact   
+	if one or more found 
+		choose one at random  
+		knock down the wall between it and CurrentCell  
+		push CurrentCell location on the CellStack  
+		make the new cell CurrentCell  
+		add 1 to VisitedCells
+	else 
+		pop the most recent cell entry off the CellStack  
+		make it CurrentCell
+	endIf
+endWhile  
+		*/
+		
+		
+		
+		return maze;
 	}
 
 	public static void main(String[] args){
@@ -237,15 +287,20 @@ class Dragon {
 	public int x = 0;
 	public int y = 0;
 	public char image = 'D';
+	public boolean isAlive = true;
 	public boolean isOnSword = false;
 	
-	public void movesToSword(){
-		image = 'F';
-		isOnSword = true;
+	public void toggleInSword(){
+		if (isOnSword){
+			image = 'D';
+			isOnSword = false;
+		} else {
+			image = 'F';
+			isOnSword = true;
+		}
 	}
 	
-	public void movesFromSword(){
-		image = 'D';
-		isOnSword = false;
+	public void die(){
+		isAlive = false;
 	}
 }
