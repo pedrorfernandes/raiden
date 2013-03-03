8c118cd23a05b646ad39a51ff9b033d39d4591f9
Diogo Santos <ds1192@hotmail.com>
2013-03-03 02:57:31 +0000

    Added eagle related maze symbols.

--- src/maze_objects/MazeSymbol.java
+++ src/maze_objects/MazeSymbol.java
@@ -1,15 +1,30 @@
 package maze_objects;
 
 public class MazeSymbol { //Symbols used in the printing functions to identify the different objects. This makes reading easier.
-	public static char dragon = 'D';
-	public static char sleepingDragon = 'd';
-	public static char armedHero = 'A';
+	
+	//Maze tiles related symbols
 	public static char sword = 'E';
 	public static char wall = 'X';
 	public static char exit = 'S';
 	public static char empty = ' ';
 	public static char space = ' ';
+	
+	//Hero related symbols
 	public static char hero = 'H';
+	public static char armedHero = 'A';
+	
+	//Dragon related symbols
+	public static char dragon = 'D';
+	public static char sleepingDragon = 'd';
+	
+	//Sword related symbols
 	public static char guardedSword = 'F';
 	public static char sleepingGuardedSword = 'f';
+	
+	//Eagle related symbols
+	public static char eagle = '>';
+	public static char eagleWithSword = 'Y';
+	public static char eagleOnWall = 'u';
+	public static char eagleOnDragon = 'G';
+	public static char eagleOnSleepingDragon = 'g';
 }
