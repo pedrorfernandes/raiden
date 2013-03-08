3aa8313a1c4b530e309761f6360a31c8c7132232
Diogo Santos <ds1192@hotmail.com>
2013-03-08 17:16:28 +0000

    First test, testing hero movement through empty spaces, implemented. It goes up, down, left, right then a more complex series of movements through the whole map.

--- src/game/test/GameTestBasics.java
+++ src/game/test/GameTestBasics.java
@@ -14,7 +14,7 @@ import maze_objects.Dragon;
 import org.junit.Test;
 
 public class GameTestBasics {
-	
+
 	private Stack<Character> createMovesStack(String moves){
 		Stack<Character> movesStack = new Stack<Character>();
 		char[] chars = moves.toCharArray();
@@ -23,7 +23,7 @@ public class GameTestBasics {
 			movesStack.push(chars[i]);
 			i--;
 		}
-	
+
 		return movesStack;
 	}
 
@@ -36,16 +36,83 @@ public class GameTestBasics {
 		dragons.add(d1);
 		dragons.add(d2);
 		dragons.add(d3);
-		
+
 		int hero_row = 1, hero_column = 1, sword_row = 7, sword_column = 8;
-		
+
 		GameOptions customOptions = new GameOptions(0, false, hero_row, hero_column, sword_row, sword_column, dragons);
 
-		Stack<Character> heroMoves = createMovesStack("das");
-		
+		Stack<Character> heroMoves = createMovesStack("ddddas");
+
 		TestInterface test = new TestInterface(customOptions, heroMoves);
 		test.startGame();
 
+	}
+
+	@Test
+	public void testHeroToEmptySpace() {
+		Dragon d1 = new Dragon(1,  7,  Dragon.STATIC);
+		ArrayList<Dragon>dragons = new ArrayList<Dragon>(1);
+		dragons.add(d1);
+
+		int hero_row = 1, hero_column = 1, sword_row = 7, sword_column = 8;
+		GameOptions customOptions = new GameOptions(0, false, hero_row, hero_column, sword_row, sword_column, dragons);
+
+		//Tests moving right to an empty space
+		Stack<Character> heroMoves = createMovesStack("d");
+
+		TestInterface test = new TestInterface(customOptions, heroMoves);
+		test.startGame();
+
+		assertEquals(1, test.getGame().getHero().getRow());
+		assertEquals(2, test.getGame().getHero().getColumn());
+
+		//Tests moving down to an empty space
+		heroMoves = createMovesStack("s");
+
+		test = new TestInterface(customOptions, heroMoves);
+		test.startGame();
+
+		assertEquals(2, test.getGame().getHero().getRow());
+		assertEquals(1, test.getGame().getHero().getColumn());
+
+		//Tests moving up to an empty space
+		hero_row = 8;
+		hero_column = 1;
+		customOptions = new GameOptions(0, false, hero_row, hero_column, sword_row, sword_column, dragons);
+
+		heroMoves = createMovesStack("w");
+
+		test = new TestInterface(customOptions, heroMoves);
+		test.startGame();
+
+		assertEquals(7, test.getGame().getHero().getRow());
+		assertEquals(1, test.getGame().getHero().getColumn());
+
+		//Tests moving left to an empty space
+		hero_row = 8;
+		hero_column = 8;
+		customOptions = new GameOptions(0, false, hero_row, hero_column, sword_row, sword_column, dragons);
+
+		heroMoves = createMovesStack("a");
+
+		test = new TestInterface(customOptions, heroMoves);
+		test.startGame();
+
+		assertEquals(8, test.getGame().getHero().getRow());
+		assertEquals(7, test.getGame().getHero().getColumn());
+
+		//Tests a full sequence of moves through empty spaces
+		hero_row = 1;
+		hero_column = 1;
+		customOptions = new GameOptions(0, false, hero_row, hero_column, sword_row, sword_column, dragons);
+
+		heroMoves = createMovesStack("dddssssswddsssaddd");
+
+		test = new TestInterface(customOptions, heroMoves);
+		test.startGame();
+
+		assertEquals(8, test.getGame().getHero().getRow());
+		assertEquals(8, test.getGame().getHero().getColumn());
 	}
 
 }
