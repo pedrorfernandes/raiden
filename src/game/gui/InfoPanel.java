eb68f3438a8dd94a96639bc6dca4cf876db6dd8d
Diogo Santos <ds1192@hotmail.com>
2013-04-10 00:12:45 +0100

    Fixed noobiness on infoPanel regarding to layouts and random bounds on the text field.

--- src/game/gui/InfoPanel.java
+++ src/game/gui/InfoPanel.java
@@ -1,7 +1,9 @@
 package game.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
@@ -15,18 +17,17 @@ public class InfoPanel extends JPanel {
 	public JTextPane textPane;
 	
 	public InfoPanel(Dimension dimension) {
+		setLayout(new BorderLayout());
 		setBorder(UIManager.getBorder("TextField.border"));
 		setPreferredSize(dimension);
-		setLayout(null);
 		
 		textPane = new JTextPane();
 		textPane.setEditable(false);
-		textPane.setBounds(21, 11, 359, 86);
 		textPane.setText("Welcome, challenger");
 		textPane.setAlignmentX(CENTER_ALIGNMENT);
 		textPane.setAlignmentY(CENTER_ALIGNMENT);
 		textPane.setBackground(null);
 		textPane.setFocusable(false);
-		add(textPane);
+		add(textPane, BorderLayout.CENTER);
 	}
 }
