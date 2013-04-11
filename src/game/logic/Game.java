f50bebddea219b0ec896631f3bbf8b000549f931
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-04-11 09:44:25 +0100

    the game should start by default as a gui with 0 args, only with arguments can the cli be started

--- src/game/logic/Game.java
+++ src/game/logic/Game.java
@@ -315,24 +315,25 @@ public class Game implements java.io.Serializable {
 	 */
 	public static void main(String[] args) {
 		
-		if(args.length != 1) {
-			System.out.println("Wrong number of arguments to call the program. Use -help for calling instructions!\n");
-			System.exit(0);
-		}
-
-		if(args[0].equals("-h") || args[0].equals("-help"))
-			System.out.println("Use -cli to start the command line interface, -gui to start the graphical interface.\n");
-		else if(args[0].equals("-cli")) {
-			CLInterface cli = new CLInterface();
-			cli.startGame();
-		}
-		else if(args[0].equals("-gui")) {
+		if(args.length == 0) {
 			GUInterface gui = new GUInterface();
 			gui.startGame();
 		}
 		else {
-			System.out.println("Wrong usage! Call with -help to check calling instructions.\n");
-			System.exit(0);
+			if(args.length != 1){
+				System.out.println("Wrong number of arguments to call the program. Use --help for calling instructions!\n");
+				System.exit(0);
+			}
+			if(args[0].equals("--h") || args[0].equals("--help"))
+				System.out.println("Use -cli to start the command line interface, -gui to start the graphical interface.\n");
+			else if(args[0].equals("-cli")) {
+				CLInterface cli = new CLInterface();
+				cli.startGame();
+			}
+			else {
+				System.out.println("Wrong usage! Call with --help to check calling instructions.\n");
+				System.exit(0);
+			}
 		}
 	}
 
