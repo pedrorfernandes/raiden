f9d54e5ef7b1aec3a14ace5c85775b41df3265ce
Diogo Santos <ds1192@hotmail.com>
2013-04-12 02:43:36 +0100

    Removed unneeded key getting code on the MazePanel class and documented it.

--- src/game/ui/gui/MazePanel.java
+++ src/game/ui/gui/MazePanel.java
@@ -6,19 +6,33 @@ import game.ui.MazePictures;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
+/**
+ * This class, extending JPanel, serves the purpose of drawing the game on the main game window in the
+ * Graphical user interface. It overrides the paintComponent method to use the printGame function of
+ * the GameOutput class.
+ */
 public class MazePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
+	/** The game to be painted on the panel */
 	Game game;
+	
+	/** The pictures to be used on the drawing. */
 	MazePictures pictures;
 
-	private LinkedList<Character> keys = new LinkedList<Character>();
-
+	/**
+	 * Instantiates a new MazePanel with the given game variable, images to be used and dimensions
+	 * for the panel.
+	 *
+	 * @param game the game variable
+	 * @param pictures the image files
+	 * @param preferred the preferred size for the JPanel
+	 * @param maximum the maximum size for the JPanel
+	 */
 	public MazePanel(Game game, MazePictures pictures, Dimension preferred, Dimension maximum) {
 		setBackground(null);
 		setFocusable(true);
@@ -30,19 +44,14 @@ public class MazePanel extends JPanel {
 		this.pictures = pictures;
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Dimension size = this.getSize();
 		GameOutput.printGame(game, g, pictures, size);
 	}
-
-	public char getNextKey() {
-		if (keys.isEmpty() ) 
-			return '\n';
-		else 
-			return keys.removeFirst();
-	}
-	
 }
 
