6d4dbdf8061135334e3eb68bfa0d57913e692e97
Diogo Santos <ds1192@hotmail.com>
2013-05-14 17:11:30 +0100

    Added test prototypes.

---
+++ RaidenTest/src/com/raiden/test/CollisionTests.java
@@ -0,0 +1,25 @@
+package com.raiden.test;
+
+import android.test.AndroidTestCase;
+
+public class CollisionTests extends AndroidTestCase {
+	
+	public void testEnemyOnPlayerCollision() {
+		/* This is going to test collisions of an enemy ship with the player ship.
+		 * It should check if the enemy receives damage as well as the player.
+		 */
+	}
+	
+	public void testEnemyBulletOnPlayerShipCollision() {
+		/* This is going to test collisions of enemy bullets against the player ship.
+		 * It should check if the player receives damage from those bullets.
+		 */
+	}
+	
+	public void testPlayerBulletOnEnemyShipCollision() {
+		/* This is going to test collisions of player bullets against the enemy ships.
+		 * It should check if the enemy receives damage from those bullets.
+		 */
+	}
+
+}
